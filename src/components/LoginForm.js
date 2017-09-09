84f7e0a53e12264d78331ccc4b3364f99733127c
Julia Kim <jkim0620@gmail.com>
2017-09-09 03:14:40 -0400

    Create input component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+// import { View, TextInput } from 'react-native';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+  state = {
+    email: '',
+    password: ''
+  };
+
   render() {
     return (
       <Card>
         <CardSection>
+          <Input
+            value={this.state.email}
+            onChangeText={text => this.setState({ email: text })}
+            label="Email"
+            placeholder="user@gmail.com"
+          />
         </CardSection>
 
         <CardSection>
+          <Input
+            value={this.state.password}
+            placeholder="password"
+            label="Password"
+            onChangeText={text => this.setState({ password: text })}
+            secureTextEntry={true}
+          />
         </CardSection>
 
         <CardSection>
