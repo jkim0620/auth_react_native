13ad0c53a1e060229c4927a97f5ed7f4492780ed
Julia Kim <jkim0620@gmail.com>
2017-09-08 23:46:19 -0400

    Setup firebase

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDYdYbGe2vg6mVPuUvaU_iGQUw4k9kN-pQ",
+      authDomain: "auth-3aac8.firebaseapp.com",
+      databaseURL: "https://auth-3aac8.firebaseio.com",
+      projectId: "auth-3aac8",
+      storageBucket: "auth-3aac8.appspot.com",
+      messagingSenderId: "73890567081"
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>
-          An App!
-        </Text>
+        <LoginForm />
       </View>
     );
   }
