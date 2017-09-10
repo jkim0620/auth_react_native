3e08227826117edf60e0a1a3ace8c89a47220d01
Julia Kim <jkim0620@gmail.com>
2017-09-09 21:17:45 -0400

    Add logout function

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = {
+    loggedIn: null
+  };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: "AIzaSyDYdYbGe2vg6mVPuUvaU_iGQUw4k9kN-pQ",
@@ -14,16 +18,41 @@ class App extends Component {
       storageBucket: "auth-3aac8.appspot.com",
       messagingSenderId: "73890567081"
     });
+
+    firebase.auth().onAuthStateChanged((user) => { //When user signs in or signs out, this function will be called
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+         return (
+           <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+           </CardSection>
+         );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
+
 }
 
 export default App;
